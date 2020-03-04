b7f1470088a88f6ab1052e4972623acccc9f7a86
Carsten Behmel <ca-behmel@t-online.de>
2020-03-04 21:25:25 +0100

    crate Menu.blade

--- resources/views/adminArea.blade.php
+++ resources/views/adminArea.blade.php
@@ -4,6 +4,6 @@
 
     <div class                   ="container-fluid">
         <div class                 ="row justify-content-md-center">
-            <h1> Backend Dashboard </h1>
+            <h1> Admin Area </h1>
 
 @endsection
