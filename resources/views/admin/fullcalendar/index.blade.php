09cae80a24556ed2101df932a727665101a27cee
Carsten Behmel <ca-behmel@t-online.de>
2020-08-07 18:23:45 +0200

    http -> https

--- resources/views/admin/fullcalendar/index.blade.php
+++ resources/views/admin/fullcalendar/index.blade.php
@@ -56,15 +56,15 @@
 
 <script src="{{ asset('js/fullcalendar/core/locales/de.js') }}" ></script>
 
-<script src="http://code.jquery.com/jquery-3.4.1.min.js" ></script>
+<script src="https://code.jquery.com/jquery-3.4.1.min.js" ></script>
 
-<script src="http://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js"></script>
 
-<script src="http://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 
-<script src="http://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
 
-<script src="http://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"></script>
 
 <script src="{{ asset('js/script.js') }}" ></script>
 
