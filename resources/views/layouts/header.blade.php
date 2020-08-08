30fac91f89746ddfa4032336c335019b1aa1682e
Carsten Behmel <ca-behmel@t-online.de>
2020-08-08 13:42:19 +0200

    referrer

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -5,7 +5,7 @@
     <meta name      ="viewport" content= "width=device-width, initial-scale=1" >
     <meta name      ="keywords">
     <meta name      ="author" content="Carsten Behmel">
-
+    <meta name="referrer" content="origin">
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ config('app.name', 'Laravel') }}</title>
