d448a408a7b02a1114b3b0eb007e0766ffa30f56
Carsten Behmel <ca-behmel@t-online.de>
2020-08-09 11:42:21 +0200

    test delete

--- app/Event.php
+++ app/Event.php
@@ -12,7 +12,22 @@ class Event extends Model
 
     protected $fillable=['entry_id','name','title','start','NumberOfPeople','end','color','phone','description'];
 
+    public function getStartAttribut($value) {
 
+        $dateStart = carbon::createFormFormat('Y-m-d H:i:s',$value)->format('Y-m-d');
+        $timeStart = carbon::createFormFormat('Y-m-d H:i:s',$value)->format('Y-m-d');
+
+        return $this->start =($timeStart == '00:00:00' ? $dateStart : $value);
+
+        }
+
+        public function getEndAttribut($value) {
+
+            $dateEnd = carbon::createFormFormat('Y-m-d H:i:s',$value)->format('Y-m-d');
+            $timeEnd = carbon::createFormFormat('Y-m-d H:i:s',$value)->format('Y-m-d');
+
+            return $this->end =($timeEnd == '00:00:00' ? $dateEnd : $value);
+        }
 
     public function entry(){
         return $this -> hasOne(entry::class);
