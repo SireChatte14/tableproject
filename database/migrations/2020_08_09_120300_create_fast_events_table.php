1be08d3f904bcbbfde9c4874e1ad47bac025897f
Carsten Behmel <ca-behmel@t-online.de>
2020-08-11 11:15:48 +0200

    Änderungen verworfen

---
+++ database/migrations/2020_08_09_120300_create_fast_events_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFastEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('fast_events', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->boolean('is_booked')->default(false);
+            $table->unsignedInteger('entry_id')->nullable();
+            $table->string('name');
+            $table->string('title');
+            $table->string('NumberOfPeople');
+            $table->dateTime('start');
+            $table->dateTime('end');
+            $table->string('color', 7);
+            $table->string('email')->nullable();
+            $table->string('phone',25);
+            $table->longText('description')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('fast_events');
+    }
+}
