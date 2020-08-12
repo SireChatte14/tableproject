481db2c9d15df2f3ec457fb8d6955aa20bb0bf3d
Carsten Behmel <ca-behmel@t-online.de>
2020-08-12 20:09:10 +0200

    delete

--- resources/views/admin/fullcalendar/index.blade.php
+++ resources/views/admin/fullcalendar/index.blade.php
@@ -60,7 +60,7 @@
 
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/popper.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"></script>
 
