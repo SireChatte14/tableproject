ef822a46ce4c7ca17d36e6a7f1580e06b75e9589
Carsten Behmel <ca-behmel@t-online.de>
2020-08-16 17:48:10 +0200

    <meta name=referrer content=strict-origin-when-cross-origin />

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
     <meta name      ="viewport" content= "width=device-width, initial-scale=1" >
     <meta name      ="keywords">
     <meta name      ="author" content="Carsten Behmel">
-
+    <meta name="referrer" content="strict-origin-when-cross-origin" />
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ config('app.name', 'Laravel') }}</title>
