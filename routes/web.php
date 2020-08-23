8bdedc82d48e7f26d0e054b8e5f940cd9fa502e8
Carsten Behmel <ca-behmel@t-online.de>
2020-08-23 19:04:13 +0200

    Reservation
    Signed-off-by: Carsten Behmel <ca-behmel@t-online.de>

--- routes/web.php
+++ routes/web.php
@@ -44,6 +44,7 @@ Route::prefix('admin')->group (function()
         Route::resource('TableEdit', 'tableSetUpController');
         Route::resource('fullcalendar', 'FullCalendarController');
         Route::resource('User', 'UserController');
+        Route::resource('reservation', 'AdminReservationController');
         Route::get('/load-events','Eventcontroller@loadEvents')->name('routeLoadEvents');
         Route::put('/event-update','Eventcontroller@update')->name('routeEventUpdate');
         Route::post('/event-store','Eventcontroller@store')->name('routeEventStore');
